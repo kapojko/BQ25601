8ce99bc8a664067ec4e05a3f111575cbdbebc5e5
Yuriy Kapoyko <yury.kapoyko@gmail.com>
2025-12-25 23:47:42 +0300

    Data declaration done, read partially done

--- BQ25601.c
+++ BQ25601.c
@@ -1,1 +1,64 @@
 #include "BQ25601.h"
+
+#define BQ25601_I2C_TIMEOUT 1
+
+bool BQ25601_Read(struct BQ25601_Platform *platform, struct BQ25601_Data *data, int reg) {
+    int ret;
+
+    // Read register data
+    if (reg == BQ25601_ALL_REGS) {
+        ret = platform->i2cReadReg(BQ25601_I2C_ADDRESS, 0, data->reg, BQ25601_REG_NUMBER,
+                                   BQ25601_I2C_TIMEOUT);
+    } else {
+        ret =
+            platform->i2cReadReg(BQ25601_I2C_ADDRESS, reg, &data->reg[reg], 1, BQ25601_I2C_TIMEOUT);
+    }
+
+    if (ret < 0) {
+        return false;
+    }
+
+    // Decode register data
+
+    // REG00
+    if (reg == 0 || reg == BQ25601_ALL_REGS) {
+        data->hizEnable = ((data->reg[0] >> 7) & 0x1) == 1;
+        data->statPinEnable = ((data->reg[0] >> 5) & 0x3) == 0;
+        data->inputCurrentLimit_mA = (data->reg[0] & 0x1F) * 100 + 100;
+    }
+
+    // REG01
+    if (reg == 1 || reg == BQ25601_ALL_REGS) {
+        data->pfmEnable = ((data->reg[1] >> 7) & 0x1) == 0;
+        data->watchdogTimerReset = ((data->reg[1] >> 6) & 0x1) == 1;
+        data->otgEnable = ((data->reg[1] >> 5) & 0x1) == 1;
+        data->chargeEnable = ((data->reg[1] >> 4) & 0x1) == 1;
+        data->systemMinimumVoltage = (enum BQ25601_SystemMinimumVoltage)((data->reg[1] >> 1) & 0x7);
+        data->minimumBatteryVoltageForOTG =
+            (enum BQ25601_MinimumBatteryVoltageForOTG)(data->reg[1] & 0x1);
+    }
+
+    // REG02
+    if (reg == 2 || reg == BQ25601_ALL_REGS) {
+        data->boostLimit = (enum BQ25601_BoostLimit)((data->reg[2] >> 7) & 0x1);
+        data->q1FullOn = ((data->reg[2] >> 6) & 0x1) == 1;
+        data->fastChargeCurrent_mA = (data->reg[2] & 0x3F) * 60;
+    }
+
+    // REG03
+    if (reg == 3 || reg == BQ25601_ALL_REGS) {
+        data->prechargeCurrent_mA = ((data->reg[3] >> 4) & 0xF) * 60 + 60;
+        data->terminationCurrent_mA = (data->reg[3] & 0xF) * 60 + 60;
+    }
+
+    // REG04
+    if (reg == 4 || reg == BQ25601_ALL_REGS) {
+        data->chargeVoltage_mV = ((data->reg[4] >> 3) & 0x1F) * 32 + 3856;
+        data->topOffTimer = (enum BQ25601_TopOffTimer)((data->reg[4] >> 1) & 0x3);
+        data->rechargeThreshold = (enum BQ25601_RechargeThreshold)(data->reg[4] & 0x1);
+    }
+
+    return true;
+}
+
+bool BQ25601_Write(struct BQ25601_Platform *platform, struct BQ25601_Data *data, int reg) {}
--- BQ25601.h
+++ BQ25601.h
@@ -6,6 +6,11 @@
 
 #define BQ25601_I2C_ADDRESS 0x6B
 
+#define BQ25601_PRODUCT_NUMBER 0x2
+
+#define BQ25601_REG_NUMBER 12
+#define BQ25601_ALL_REGS (-1)
+
 enum BQ25601_SystemMinimumVoltage {
     BQ25601_SYS_MIN_2_6V = 0x0,
     BQ25601_SYS_MIN_2_8V = 0x1,
@@ -18,7 +23,7 @@ enum BQ25601_SystemMinimumVoltage {
 };
 
 // Minimum battery voltage for OTG mode.
-enum BQ25601_MinVBatSel_OTG {
+enum BQ25601_MinimumBatteryVoltageForOTG {
     BQ25601_MIN_VBAT_SEL_2_8V_FALL = 0,
     BQ25601_MIN_VBAT_SEL_2_5V_FALL = 1,
 };
@@ -82,7 +87,7 @@ enum BQ25601_JEITA_VSET {
     BQ25601_JEITA_VSET_VREG = 1,
 };
 
-enum BQ25601_TrackBat {
+enum BQ25601_VindpmTrackBatteryVoltage {
     BQ25601_VDPM_BAT_TRACK_DISABLE = 0x0,
     BQ25601_VDPM_BAT_TRACK_VBAT_PLUS_200MV = 0x1,
     BQ25601_VDPM_BAT_TRACK_VBAT_PLUS_250MV = 0x2,
@@ -117,5 +122,95 @@ enum BQ25601_NTC_Fault {
     BQ25601_NTC_FAULT_COLD = 0x5,
     BQ25601_NTC_FAULT_HOT = 0x6,
 };
+
+struct BQ25601_Platform {
+    int (*i2cWriteReg)(uint8_t addr7bit, uint8_t regNum, const uint8_t *data, uint8_t length, uint8_t wait);
+    int (*i2cReadReg)(uint8_t addr7bit, uint8_t regNum, uint8_t *data, uint8_t length, int timeout);
+    void (*debugPrint)(const char *fmt, ...);
+};
+
+struct BQ25601_Data {
+    uint8_t reg[BQ25601_REG_NUMBER];
+
+    // REG00, all R/W
+    bool hizEnable; // Enable HIZ Mode
+    bool statPinEnable; // Enable STAT Pin function
+    uint16_t inputCurrentLimit_mA; // Input Current Limit
+
+    // REG01, all R/W
+    bool pfmEnable; // Enable PFM
+    bool watchdogTimerReset; // I2C Watchdog Timer Reset, true - reset
+    bool otgEnable; // Enable OTG
+    bool chargeEnable; // Enable Charge
+    enum BQ25601_SystemMinimumVoltage systemMinimumVoltage; // System Minimum Voltage
+    enum BQ25601_MinimumBatteryVoltageForOTG minimumBatteryVoltageForOTG; // Minimum battery voltage for OTG mode
+
+    // REG02, all R/W
+    enum BQ25601_BoostLimit boostLimit; // Boost current limit
+    bool q1FullOn; // Use lower Q1 RDSON always (better efficiency), except boost mode
+    uint16_t fastChargeCurrent_mA; // Fast Charge Current
+
+    // REG03, all R/W
+    uint16_t prechargeCurrent_mA; // Precharge Current
+    uint16_t terminationCurrent_mA; // Termination Current
+
+    // REG04, all R/W
+    uint16_t chargeVoltage_mV; // Charge Voltage
+    enum BQ25601_TopOffTimer topOffTimer; // The extended time following the termination condition is met
+    enum BQ25601_RechargeThreshold rechargeThreshold; // Recharge Threshold
+
+    // REG05, all R/W
+    bool terminationEnable; // Enable termination
+    enum BQ25601_WatchdogTimer watchdogTimer; // Watchdog Timer
+    bool chargeTimerEnable; // Enable both fast charge and precharge timer
+    enum BQ25601_ChargeTimer chargeTimer; // Charge Timer
+    enum BQ25601_ThermalRegulationThreshold thermalRegulationThreshold; // Thermal Regulation Threshold
+    enum BQ25601_JEITA_ISET jeitaIset; // JEITA ISET
+
+    // REG06, all R/W
+    enum BQ25601_VAC_OVP_Threshold vacOvpThreshold; // VAC OVP Threshold
+    enum BQ25601_BoostRegulationVoltage boostRegulationVoltage; // Boost Regulation Voltage
+    uint16_t absoluteVindpmThreshold_mV; // Absolute VINDPM Threshold
+
+    // REG07, all R/W
+    bool inputCurrentLimitDetection; // Force input current limit detection when VBUS is present
+    bool safetyTimerSlowed2X; // Safety timer slowed by 2X during input DPM (both V and I) or JEITA cool, or thermal regulation
+    bool q4TurnOff; // Turn off Q4 with tBATFET_DLY delay time
+    enum BQ25601_JEITA_VSET jeitaVset; // JEITA VSET
+    bool batfetDelay; // Turn off BATFET after tBATFET_DLY (typ. 10 s)
+    bool batfetResetEnable; // Enable BATFET reset function
+    enum BQ25601_VindpmTrackBatteryVoltage vindpmTrackBatteryVoltage; // Sets VINDPM to track BAT voltage
+
+    // REG08, all R
+    enum BQ25601_VBusStatus vBusStatus; // VBUS Status register
+    enum BQ25601_ChargingStatus chargingStatus; // Charging status
+    bool powerGood; // Power Good Status
+    bool thermalRegulationStatus; // Thermal Regulation Status
+    bool vsysMinRegulationStatus; // In VSYS_MIN regulation (BAT < VSYS_MIN)
+
+    // REG09, all R
+    bool watchdogFault; // Watchdog timer expiration
+    bool boostFault; // any conditions that cannot start boost function
+    enum BQ25601_ChargeFault chargeFault; // Charge fault status
+    bool batteryFault; // Battery fault status
+    enum BQ25601_NTC_Fault ntcFault; // JEITA
+
+    // REG0A, all R except masks
+    bool vbusAttached; // VBUS Attached
+    bool vindpmStatus; // in VINDPM
+    bool iindpmStatus; // in IINDPM
+    bool topOffActive; // Top off timer counting
+    bool acovStatus; // Device is in ACOV
+    bool vindpmIntMask; // Mask VINDPM INT pulse
+    bool iindpmIntMask; // Mask IINDPM INT pulse
+
+    // REG0B, all R except reset
+    bool reset; // Reset to default register value and reset safety timer
+    uint8_t productNumber; // Product Number, BQ25601_PRODUCT_NUMBER
+    uint8_t deviceRevision; // Device Revision
+};
+
+bool BQ25601_Read(struct BQ25601_Platform *platform, struct BQ25601_Data *data, int reg);
+bool BQ25601_Write(struct BQ25601_Platform *platform, struct BQ25601_Data *data, int reg);
 
 #endif // BQ25601_H
